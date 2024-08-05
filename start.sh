4d6a667fd23e376260250c23e3a16a6f77962ac0
Kizitor Chukwuma <kizitorlive@hotmail.com>
2024-08-04 20:54:10 -0500

    IP Change because of Docker Issues on WSL

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 export FLASK_RUN_PORT=80
-export FLASK_RUN_HOST="127.0.0.1"
+export FLASK_RUN_HOST="0.0.0.0"
 export CON_CSV="./Assignment_Docs/concentration.timeseries.csv"
 
 flask --app ConcentrationController run
